40bd3a42c441f7b719d1e9deeb6f3becfbd81ffc
Leva <novikovlev48@gmail.com>
2026-04-18 08:58:13 +0200

    sdffasdf

--- LinearRegressionModel.py
+++ LinearRegressionModel.py
@@ -16,11 +16,12 @@ class Model_LR:
 
 
     def fit(self, features_train, labels_train, features_val, labels_val):
-        self.weights = np.zeros(len(features_train))
+        self.weights = np.zeros(len(features_train[0]))
+        self.weights_history = []
         self.losses_history = []
         self.losses_val_history = []
 
-        for epoch in self.epochs:
+        for epoch in range(self.epochs):
 
             for i in range(0, len(labels_train), self.batch_size):
                 features_train_batch = features_train[i : i + self.batch_size]
@@ -28,10 +29,11 @@ class Model_LR:
 
                 self.update_weights(features_train_batch, labels_train_batch)
 
-            self.update_losses(features_train, labels_train)
+                self.update_losses(features_train_batch, labels_train_batch, features_val, labels_val)
+                
     
     def update_weights(self, features_batch, labels_batch):
-        prediction = self.predict(features_batch, labels_batch)
+        prediction = self.predict(features_batch)
         loss = prediction - labels_batch
         n = len(labels_batch)
 
@@ -39,16 +41,18 @@ class Model_LR:
         db = 1 / n * np.sum(loss)
 
         self.weights -= self.lr * dw
-        self.bias -= self.r * db
+        self.bias -= self.lr * db
+
+        self.weights_history.append(self.weights)
 
 
-    def predict(self, features, labels):
+    def predict(self, features):
         return np.dot(features, self.weights) + self.bias
     
     
     def update_losses(self, features, labels, features_val, labels_val):
         # normal history
-        prediction = self.predict(features, labels)
+        prediction = self.predict(features)
         loss = prediction - labels
         
         MSE = np.mean(loss ** 2)
@@ -56,8 +60,8 @@ class Model_LR:
 
 
         # validation historyprediction = self.predict(features, labels)
-        if (features_val != None and labels_val != None):
-            prediction = self.predict(features_val, labels_val)
+        if (features_val.all() != None and labels_val.all() != None):
+            prediction = self.predict(features_val)
             loss = prediction - labels_val
 
             MSE = np.mean(loss ** 2)
@@ -66,17 +70,16 @@ class Model_LR:
 
 
     def test_model(self, features_test, labels_test):
-        predictions = self.predict(features_test, labels_test)
+        predictions = self.predict(features_test)
         loss = predictions - labels_test
 
         MSE = np.mean(loss ** 2)
         return MSE
         
 
-    def paint_data(self, features, labels, fig, axis, row):
-        predictions = self.predict(features, labels)
-        n = len(labels)
+    def paint_data(self, features_train, labels_train, ftrs_val, lbls_val, ftrs_test, lbls_test, fig, axis, row):
+        predictions = self.predict(ftrs_test)
 
-        w, b = np.polyfit(predictions, labels, 1)
-        axis[row][0].scatter(predictions, labels)
-        axis[row][0].plot([0, b], [w * n + b], color = 'red')
+        w, b = np.polyfit(predictions, lbls_test, 1)
+        axis[row][0].scatter(predictions, lbls_test)
+        axis[row][0].plot([0, b], [w * len(lbls_test) + b], color = 'red')
--- main.py
+++ main.py
@@ -7,16 +7,30 @@ from LinearRegressionModel import Model_LR as model
 
 fig, ax = plt.subplots(2, 3)
 
-dataset = sklearn.datasets.fetch_california_housing().data
 # ['MedInc', 'HouseAge', 'AveRooms', 'AveBedrms', 'Population', 'AveOccup', 'Latitude']
-
+dataset = sklearn.datasets.fetch_california_housing().data
 labels = dataset[:, 2] # average rooms
-
-print(dataset)
-print(labels[:20])
 
 # Labels - AveRooms
 
+learning_rate = 0.00001
+epochs = 100
+batch_size = 128
+
 # first model. Using all features
+
+val_point = 15000
+test_point = 18000
 features = np.delete(dataset, 2, axis=1)
-print(features[0])
+
+ftrs_train = features[:val_point]
+lbls_train = labels[:val_point]
+ftrs_val = features[val_point : test_point]
+lbls_val = labels[val_point : test_point]
+ftrs_test = features[test_point:]
+lbls_test = labels[test_point:]
+
+model_all_features = model(learning_rate, epochs, batch_size)
+model_all_features.fit(ftrs_train, lbls_train, ftrs_val, lbls_val)
+model_all_features.paint_data(ftrs_train, lbls_train, ftrs_val, lbls_val, ftrs_test, lbls_test,
+                              fig, ax, 0)
